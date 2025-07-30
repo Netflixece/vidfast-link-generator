9308aa348210b2a4c87b59ed7cf1df221cbaf40f
Netflixece <137390410+Netflixece@users.noreply.github.com>
2025-07-30 19:28:08 +0530

    Update Icons.tsx

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -157,3 +157,13 @@ export const ArrowUpIcon = ({ className }: { className?: string }) => (
         <polyline points="5 12 12 5 19 12"></polyline>
     </svg>
 );
+
+export const PaletteIcon = ({ className }: { className?: string }) => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={className}>
+        <circle cx="13.5" cy="6.5" r="2.5"></circle>
+        <circle cx="17.5" cy="10.5" r="2.5"></circle>
+        <circle cx="8.5" cy="7.5" r="2.5"></circle>
+        <circle cx="6.5" cy="12.5" r="2.5"></circle>
+        <path d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10c.926 0 1.648-.746 1.648-1.667 0-.422-.02-.833-.052-1.222"></path>
+    </svg>
+);
