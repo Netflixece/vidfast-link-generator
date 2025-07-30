3190940369ffb6797a9a8ad8bf240d5f9b0cd600
Netflixece <137390410+Netflixece@users.noreply.github.com>
2025-07-30 15:14:19 +0530

    Update ResultsGrid.tsx

--- components/ResultsGrid.tsx
+++ components/ResultsGrid.tsx
@@ -1,4 +1,5 @@
 
+
 import React from 'react';
 import type { SearchResult } from '../types';
 import ResultCard from './ResultCard';
@@ -11,8 +12,14 @@ interface ResultsGridProps {
 const ResultsGrid: React.FC<ResultsGridProps> = ({ results, onSelect }) => {
   return (
     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-6">
-      {results.map((item) => (
-        <ResultCard key={`${item.media_type}-${item.id}`} item={item} onSelect={onSelect} />
+      {results.map((item, index) => (
+        <div
+          key={`${item.media_type}-${item.id}`}
+          className="opacity-0 animate-fade-in-up"
+          style={{ animationDelay: `${index * 75}ms` }}
+        >
+          <ResultCard item={item} onSelect={onSelect} />
+        </div>
       ))}
     </div>
   );
